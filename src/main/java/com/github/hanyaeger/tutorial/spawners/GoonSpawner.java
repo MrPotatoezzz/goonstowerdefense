33b467ecd7eace0073ac3b85a85466fb87356300
MrPotatoezzz <93940585+MrPotatoezzz@users.noreply.github.com>
2022-03-28 11:50:17 +0200

    Goonspawner toegevoegd

---
+++ src/main/java/com/github/hanyaeger/tutorial/spawners/GoonSpawner.java
@@ -0,0 +1,21 @@
+package com.github.hanyaeger.tutorial.spawners;
+import com.github.hanyaeger.api.entities.EntitySpawner;
+import com.github.hanyaeger.tutorial.entities.Goons.Goon;
+import com.github.hanyaeger.tutorial.scenes.Map1;
+
+
+public class GoonSpawner extends EntitySpawner {
+
+    private Map1 level;
+
+    public GoonSpawner(long intervalInMs, Map1 level) {
+        super(intervalInMs);
+        this.level = level;
+    }
+
+
+    @Override
+    protected void spawnEntities() {
+        spawn(level.spawnGoon());
+    }
+}
